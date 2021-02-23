a34e00b3287b67babd3e509b8805274e24470aea
Rhea Rodrigues <rhea49rdgs@gmail.com>
2021-02-23 15:37:52 -0800

    added ./public to .gitignore

---
+++ src/pages/index.js
@@ -0,0 +1,27 @@
+// Gatsby supports TypeScript natively!
+import React from "react"
+import { graphql } from "gatsby"
+
+import Bio from "../components/bio"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+ export default function BlogIndex ({ data, location }) {
+  const siteTitle = data.site.siteMetadata.title
+  return (
+    <Layout location={location} title={siteTitle}>
+      <SEO title="All posts" />
+      <Bio />
+    </Layout>
+  )
+}
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
