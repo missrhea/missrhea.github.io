fbb0381ab52ff0ed35cc39ae3e3093c443516d2b
Rhea Rodrigues <rhea49rdgs@gmail.com>
2021-03-04 17:50:44 -0800

    adeed a bio

--- src/components/bio.js
+++ src/components/bio.js
@@ -79,9 +79,19 @@ const Bio = () => {
           </a>
       </div>
 
-      <h2>Hi!</h2>
-      <p>a bio</p>
-      
+      <h3>Hi there!</h3>
+      <p className="bio">
+
+My name is Rhea. In December 2020 I graduated with a Master's degree in <a href="https://www.sfu.ca/big-data/big-data-sfu">Computer Science and specialization in Big Data</a> from <a href="https://www.sfu.ca/">SFU</a>. Before that, I was pursuing my Bachelor's degree in Computer Engineering.  I'm a data scientist and software engineer. Python and SQL are my bread and butter. Last summer I was the data scientist on my team for <a href="https://jobs.rbc.com/ca/en/amplify">RBC Amplify</a>, a high intensity innovation program. 
+<br></br>
+I LOVE data of all sorts, big or small! My area of expertise is building robust machine learning and statistical methods to solve real-world challenges. I have experience working with cloud computing services to scale and deploy models.  Besides this, data viz fascinates me, and I sometimes tinker around with <a href="https://d3js.org">D3.js</a> for fun. 
+<br></br>
+I enjoy collaborating on teams as much as I value ownership and working independently. I'm always curious and that fuels my love for learning. I have a desire to work on complex, tough, meaningful problems. I'm an optimist. A better product, a better team, a better life, or a better world is always inspiring to me. 
+<br></br>
+      </p>
+      <h3>👋🏻 Say hi</h3>
+Feel free to reach out to on email or DM me on twitter/linkedin.
+
     </div>
   )
 }
