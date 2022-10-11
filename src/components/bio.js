9478b463fc54e7ecdc4b7d7d1b95c388e96612ff
Rhea Rodrigues <rhea49rdgs@gmail.com>
2022-10-10 17:11:04 -0700

    new Biogit statusgit statusgit status

--- src/components/bio.js
+++ src/components/bio.js
@@ -79,19 +79,17 @@ const Bio = () => {
           </a>
       </div>
 
-      <h3>Hi there!</h3>
       <p className="bio">
-
-My name is Rhea. In December 2020 I graduated with a Master's degree in <a href="https://www.sfu.ca/big-data/big-data-sfu">Computer Science and specialization in Big Data</a> from <a href="https://www.sfu.ca/">SFU</a>. Before that, I was pursuing my Bachelor's degree in Computer Engineering.  I'm a data scientist and software engineer. Python and SQL are my bread and butter. Last summer I was the data scientist on my team for <a href="https://jobs.rbc.com/ca/en/amplify">RBC Amplify</a>, a high intensity innovation program. 
+Hi there! My name’s Rhea. I have over two years of experience designing, building, and deploying machine learning systems at scale. I have a background in engineering and business, and I’m based in Vancouver, Canada.
 <br></br>
-I LOVE data of all sorts, big or small! My area of expertise is building robust machine learning and statistical methods to solve real-world challenges. I have experience working with cloud computing services to scale and deploy models.  Besides this, data viz fascinates me, and I sometimes tinker around with <a href="https://d3js.org">D3.js</a> for fun. 
+Currently, I’m a Data Scientist on the Global Cyber Security team at RBC where I ship Machine Learning and Deep Learning solutions to minimize the risk of insider threats and data exposure.
 <br></br>
-I enjoy collaborating on teams as much as I value ownership and working independently. I'm always curious and that fuels my love for learning. I have a desire to work on complex, tough, meaningful problems. I'm an optimist. A better product, a better team, a better life, or a better world is always inspiring to me. 
+Previously, I was on the Data and Analytics team at RBC where I developed Machine Learning models to improve the client experience with product recommendations and increased client acquisition and engagement, for 12M+ clients in collaboration with Retail Banking and Wealth Management.
 <br></br>
+I value collaboration as much as I value ownership and working independently. I'm always curious and that fuels my love for learning. I want to work on meaningful problems, especially challenging ones that help me grow. I'm an optimist. A better product, a better team, a better life, or a better world is always inspiring to me.
+<br></br>
+Listening to podcasts on a walk, reading (check out my reviews), hiking, and teaching are some things I enjoy doing in my free time. Feel free to reach out to me via email or hit me up on Twitter/LinkedIn.
       </p>
-      <h3>👋🏻 Say hi</h3>
-Feel free to reach out to on email or DM me on twitter/linkedin.
-
     </div>
   )
 }
